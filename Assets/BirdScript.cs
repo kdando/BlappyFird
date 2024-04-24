31831efebe994b0742f5b9f71951f8571f7d0b53
kdando <kvdando@gmail.com>
2024-04-24 14:05:26 +0100

    game over screen with restart logic

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,21 +6,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.name = "Billy Birdman";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidBody.velocity = Vector2.up * flapStrength;
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
